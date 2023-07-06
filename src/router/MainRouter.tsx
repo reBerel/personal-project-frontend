d4725322cb9ca0ea9a2f9d864a364d804fd21027
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-07-06 17:21:41 +0900

    feat: [장현민] 초기 MainRouter설정

---
+++ src/router/MainRouter.tsx
@@ -0,0 +1,26 @@
+import React, { Suspense } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import BoardListPage from '../board/BoardListPage'
+import BoardRegisterPage from '../board/BoardRegisterPage'
+import BoardReadPage from '../board/BoardReadPage'
+import SignIn from '../sign/SignIn'
+import SignUp from '../sign/SignUp'
+
+const MainRouter = () => {
+  return (
+    <div>
+        <Suspense fallback={<div>로딩중.......</div>}>
+            <Routes>
+                <Route path='/' element={<Navigate to="/key-we-board-page/list" replace/>}/>
+                 <Route path='key-we-board-page/list' element={<BoardListPage/>} />
+                 <Route path='key-we-board-page/register' element={<BoardRegisterPage/>} />
+                 <Route path='key-we-board-page/read/:boardId' element={<BoardReadPage/>} />
+                 <Route path='key-we-board-page/sign-in' element={<SignIn/>} />
+                 <Route path='key-we-board-page/sign-up' element={<SignUp/>} />
+            </Routes>
+        </Suspense>
+    </div>
+  )
+}
+
+export default MainRouter
