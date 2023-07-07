ce3e321581ddd0af4f77dc48e8ea8660eabbe72c
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-07-07 15:01:21 +0900

    feat: 숫자페이지 컨퍼넌트 생성[KWF-PAGE-1]

---
+++ src/board/BoardPage.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Pagination from '@mui/material/Pagination';
+import { ChangeEvent } from 'react';
+
+const BoardPage = () => {
+  const [page, setPage] = useState(1);
+  const handlePageChange = (event: ChangeEvent<unknown>, page: number) => {
+    setPage(page);
+  };
+
+  return (
+    <Pagination page={page} count={10} siblingCount={2} boundaryCount={1} shape="rounded" onChange={handlePageChange} showFirstButton showLastButton sx={{marginTop: '0.5rem'}}/> 
+  );
+};
+
+export default BoardPage;
