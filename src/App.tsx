bceb1611d30861fb5c6f9b61fb901594a0a31314
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-07-07 17:52:28 +0900

    update: [장현민]하단 네비게이션 추가[KWF-APP-x]

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import MainRouter from './router/MainRouter';
 import NavigationBar from './navigation/NavigationBar'
-
+import NavigatonBottomBar from './navigation/NavigatonBottomBar'
 const App: React.FC = () =>{
   return (
     <div>
         <NavigationBar/>
         <MainRouter/>
+        <NavigatonBottomBar/>
     </div>
   );
 }
