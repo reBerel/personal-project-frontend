3b7d66aa42f9a761889fcefc5c24c2c93f546e0f
hyunmin Chang <changhs318@gmail.com>
2023-07-11 17:42:19 +0900

    fix: [장현민]반복되는 문장 Component로 변경 [KWF-LIST-F-2]

---
+++ src/board/component/BoardListGridComponent.tsx
@@ -0,0 +1,40 @@
+import { Box, Button, Container, Grid } from '@mui/material'
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import BoardPageComponent from '../component/BoardPageComponent'
+import BoardSearchComponent from '../component/BoardSearchComponent'
+
+
+const BoardListGridComponent = () => {
+  const navigate = useNavigate()
+
+  const handleRegisterClick = () => {
+  navigate('/key-we-board-page/register');
+};
+  return (
+    <Container>
+          <Grid container spacing={2}>
+            <Grid item xs={3}>
+            <Button onClick={handleRegisterClick} color="error" style={{ marginTop: '4px' }}>글 작성</Button>            
+          </Grid>
+            <Grid item xs={4}>
+            <Box/>            
+            </Grid>
+            <Grid item xs={5}>
+            <BoardSearchComponent/>
+            </Grid>                        
+            <Grid item xs={2}>
+            <Box/>            
+            </Grid>
+            <Grid item xs={8}>
+            <BoardPageComponent/>
+            </Grid>
+            <Grid item xs={2}>
+            <Box/>            
+            </Grid>
+            </Grid>
+    </Container>
+  )
+}
+
+export default BoardListGridComponent
