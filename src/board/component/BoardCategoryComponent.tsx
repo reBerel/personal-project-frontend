0a7805faed218d63a87405709df05602d2833b3e
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-07-11 16:51:30 +0900

    update: [장현민] 게시판 카테고리 변경[KWF-CATEGORY-2]

---
+++ src/board/component/BoardCategoryComponent.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import { Paper } from '@mui/material';
+
+
+export default function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <Box>
+      <BottomNavigation showLabels value={value} onChange={(event, newValue) => { setValue(newValue); }} sx={{ height: '2.5rem', marginBottom: '5px'}} component={Paper}>
+      <BottomNavigationAction label="main"  />
+        <BottomNavigationAction label="Spring"  />
+        <BottomNavigationAction label="Pytion"  />
+        <BottomNavigationAction label="Vue"  />
+        <BottomNavigationAction label="React"  />
+        <BottomNavigationAction label="Question"  />
+      </BottomNavigation>
+    </Box>
+  );
+}
