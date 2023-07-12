ec9c88cad7acccda15e264d1912d86eadeb2332b
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-07-12 17:20:01 +0900

    fix: [장현민]Navigation 분리 및 필요없는 부분 제거

--- src/navigation/NavigationBar.tsx
+++ src/navigation/NavigationBar.tsx
@@ -4,111 +4,50 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import { Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, ThemeProvider, createTheme, useTheme } from '@mui/material';
+import { Grid, ThemeProvider, useTheme } from '@mui/material';
 import { Link } from 'react-router-dom';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import MenuIcon from '@mui/icons-material/Menu';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import {Search, SearchIconWrapper, StyledInputBase, drawerWidth, AppBar, DrawerHeader } from './NavigationComponent'
+
+
+import {Search, SearchIconWrapper, StyledInputBase, AppBar } from './NavigationComponent'
 import {  } from '@mui/material/colors';
+import NavigationSideBar from './NavigationSideBar';
 
 export default function NavigationBar() {
   const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
 
   return (
     <ThemeProvider theme={theme}>
-    <Box sx={{ flexGrow: 1}}>
+ 
       <AppBar position="static" sx={{ backgroundColor: '#6BB07B' }}>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 5 }}
-          >
-          </IconButton>
+        <Toolbar>          
+        <Grid container spacing={1} sx={{marginTop: '10px'}}>
+        <Grid item xs={2}>  
+          <Box/>
+          </Grid>
+        <Grid item xs={2}>  
           <Typography color='white' variant="h6" component={Link} to="/key-we-board-page/list" style={{ textDecoration: "none" }}>
             KeyWe |
           </Typography>
-          <Box sx={{ flexGrow: 0.9}}/>
+          </Grid>
+          <Grid item xs={3.7}>  
+          <Box/>
+          </Grid>
           <Search>
-            <SearchIconWrapper />
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-          <Box>
-          <Button component={Link} to="/key-we-board-page/sign-in" color="inherit">로그인</Button>          
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="end"
-            onClick={handleDrawerOpen}
-            sx={{ ...(open && { display: 'none' }) }}
-          >
-            <MenuIcon/>
-          </IconButton>
-          </Box>
+            <SearchIconWrapper />            
+            <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} sx={{padding: '5px'}}/>
+          </Search>          
+            <Grid item xs={1.6}>
+          <Button component={Link} to="/key-we-board-page/sign-in" color="inherit">로그인</Button>                    
+          </Grid>
+          <Grid item xs={0.5}>  
+          <NavigationSideBar/>
+          </Grid>
+          <Grid item xs={2}>  
+          <Box/>
+          </Grid>
+          </Grid>
         </Toolbar>
-      </AppBar>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-          },
-        }}
-        variant="persistent"
-        anchor="right"
-        open={open}
-      >
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronLeftIcon/> : <ChevronRightIcon/>}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>      
-    </Box>
+      </AppBar>           
     </ThemeProvider>
   );
 }
--- src/navigation/NavigatonBottomBar.tsx
+++ src/navigation/NavigatonBottomBar.tsx
@@ -1,38 +1,27 @@
 
-import { BottomNavigation, BottomNavigationAction, Box, Button, styled } from "@mui/material";
+import { BottomNavigation, Box, Button, styled } from "@mui/material";
 import React from "react";
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FolderIcon from '@mui/icons-material/Folder';
-import { green, grey } from "@mui/material/colors";
 import { Link } from "react-router-dom";
 
 
 const RootContainer = styled('div')({
   width: '100%',
-  backgroundColor: '#CFDDCE',
 });
 
 const StyledBottomNavigation = styled(BottomNavigation)({
   marginTop: 'auto',
 });
 export default function LabelBottomNavigation() {
-  const [value, setValue] = React.useState('recents');
-
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
-  };
 
   return (
     <RootContainer>
-      <StyledBottomNavigation  sx={{marginTop: 'calc(20%)', bottom : 0, width: '100%',position: 'relative', transform: 'translateY(-99%)', backgroundColor: '#CFDDCE' }} >
+      <StyledBottomNavigation  sx={{position: 'relative', transform: 'translateY(0%)', display: "flex", alignItems: 'center', justifyContent: 'center',marginTop: '5rem', borderTop: '1.5px solid #6BB07B' }}>
         <Box sx={{margin: '20px'}}>
-        <Button component={Link} to="/https://github.com/reBerel" aria-label="mygithub">
+        <Button component={Link} to="https://github.com/reBerel" aria-label="mygithub">
           github
         </Button>
-        <Button component={Link} to="/key-we-board-page/list">1</Button>
-        <Button component={Link} to="/key-we-board-page/list">2</Button>
+        <Button component={Link} to="https://github.com/EDDI-RobotAcademy/personal-project-backend">backend</Button>
+        <Button component={Link} to="https://github.com/EDDI-RobotAcademy/personal-project-frontend">frontend</Button>
         </Box>
       </StyledBottomNavigation>
     </RootContainer>
