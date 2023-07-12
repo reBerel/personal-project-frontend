502eb432da4c978ce432430032101a22258ce364
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-07-12 17:22:49 +0900

    update: [장현민]firebase 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MainRouter from './router/MainRouter';
 import NavigationBar from './navigation/NavigationBar'
 import NavigatonBottomBar from './navigation/NavigatonBottomBar'
+import { firestore } from "./firebase";
 const App: React.FC = () =>{
+  useEffect(() => {
+    console.log(firestore)
+  })
   return (
+    
     <div>
+    <div style={{position: 'relative', minHeight: 100}}>
         <NavigationBar/>
         <MainRouter/>
-        <NavigatonBottomBar/>
+  
+    </div>
+    <div>
+    <NavigatonBottomBar/>
+    </div>;
     </div>
   );
 }
