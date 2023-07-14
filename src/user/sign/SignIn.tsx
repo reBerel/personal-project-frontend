a67b0be483b72d6b7c6ff73eebb0e4acc7cb8ece
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-07-14 15:06:52 +0900

    update: [장현민]로그인 초기 값 설정[KWF-SIGN_IN-2]

--- src/user/sign/SignIn.tsx
+++ src/user/sign/SignIn.tsx
@@ -1,6 +1,8 @@
 import { Box, Button, Container, Grid, Link, TextField, ThemeProvider, Typography, createTheme } from '@mui/material'
 import { green } from '@mui/material/colors';
-import React from 'react'
+import React, {  useState } from 'react'
+import springAxiosInst from '../../utility/axiosInstance';
+import { User } from '../entity/User';
 
 const theme = createTheme({
   components: {
@@ -20,17 +22,56 @@ const theme = createTheme({
 });
 
 const SignIn = () => {
+  const [inputEmail, setInputEmail] = useState('')
+  const [inputPassword, setInputPassword] = useState('')
+  const [user, setUser] = useState('')
 
-
+  const handleInputEmail = (e: any) => {
+    setInputEmail(e.target.value)
+  }
   
+  const handleInputPassword = (e: any) => {
+    setInputPassword(e.target.value)
+  }
+
+  const onClickSignIn = () => {
+    console.log('click sign-in')
+    console.log('Email: ', inputEmail)
+    console.log('Password: ', inputPassword)
+    springAxiosInst.post('/user/sign-in', {
+      user_Email: inputEmail,
+      user_Password: inputPassword
+    })
+    .then(res => {
+      const userData = res.data;
+      setUser(userData);
+      console.log(res);
+      console.log('userData.email :: ',userData.email);
+      console.log('userData.password :: ',userData.password);      
+      if (userData.email === undefined) {
+        console.log('======================',userData.password)
+        alert('입력하신 email이 일치하지 않습니다')        
+      } else if (userData.email === null) {
+        console.log('======================','입력하신 비밀번호 가 일치하지 않습니다.')
+        alert('입력하신 비밀번호 가 일치하지 않습니다.')
+      } else if (userData.email === inputEmail) {
+        console.log('======================','로그인 성공')
+        sessionStorage.setItem('email', inputEmail)
+      }
+      // document.location.href = '/key-we-board-page/sign-in'
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }    
   return (
     <ThemeProvider theme={theme}>
     <Container maxWidth="xs"  style={{ marginTop: "30px" }}>
       <Typography component="h1" variant="h5" sx={{ marginBottom: 1 }}> 로그인 </Typography>
-    <Box display="flex" flexDirection="column">
-       <TextField autoFocus label="아이디" sx={{p:0.8}}></TextField>
-       <TextField label="비밀번호" sx={{p:0.8}}></TextField>    
-       <Button style={{marginTop: "20px"}} variant="contained" sx={{backgroundColor:"#6BB07B", color: 'white'}} >로그인</Button>
+    <Box display="flex" flexDirection="column" >
+       <TextField autoFocus id='email' name='email' label="아이디" sx={{p:0.8}} value={inputEmail} onChange={handleInputEmail}/>
+       <TextField id='password' name='password' label="비밀번호" sx={{p:0.8}} value={inputPassword} onChange={handleInputPassword}/>
+       <Button onClick={onClickSignIn} style={{marginTop: "20px"}} variant="contained" sx={{backgroundColor:"#6BB07B", color: 'white'}} >로그인</Button>
        </Box >
        <Grid container>
             <Grid item xs marginTop='10px'>
