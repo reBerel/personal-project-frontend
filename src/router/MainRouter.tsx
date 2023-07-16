f0dd314002d6144855d9162e32287b070c648110
Raun0129 <63294294+reBerel@users.noreply.github.com>
2023-07-17 06:11:04 +0900

    fix: [장현민]가독성을 위한 페이지 분류

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -1,26 +1,26 @@
 import React, { Suspense } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
-import BoardListPage from '../board/BoardListPage'
-import BoardRegisterPage from '../board/BoardRegisterPage'
-import BoardReadPage from '../board/BoardReadPage'
-import SignIn from '../sign/SignIn'
-import SignUp from '../sign/SignUp'
-import BoardModifyPage from '../board/BoardModifyPage'
+import BoardListPage from '../board/page/BoardListPage'
+import BoardRegisterPage from '../board/page/BoardRegisterPage'
+import BoardReadPage from '../board/page/BoardReadPage'
+import SignIn from '../user/sign/SignIn'
+import SignUp from '../user/sign/SignUp'
+import BoardModifyPage from '../board/page/BoardModifyPage'
 
 const MainRouter = () => {
   return (
     <div>
-        <Suspense fallback={<div>로딩중.......</div>}>
-            <Routes>
-                <Route path='/' element={<Navigate to="/key-we-board-page/list" replace/>}/>
-                 <Route path='key-we-board-page/list' element={<BoardListPage/>} />
-                 <Route path='key-we-board-page/register' element={<BoardRegisterPage/>} />
-                 <Route path='key-we-board-page/read/:boardId' element={<BoardReadPage/>} />
-                 <Route path='key-we-board-page/modify/:boardId' element={<BoardModifyPage/>} />
-                 <Route path='key-we-board-page/sign-in' element={<SignIn/>} />
-                 <Route path='key-we-board-page/sign-up' element={<SignUp/>} />
-            </Routes>
-        </Suspense>
+      <Suspense fallback={<div>로딩중.......</div>}>
+        <Routes>
+          <Route path='/' element={<Navigate to="/key-we-board-page/list" replace/>}/>
+          <Route path='key-we-board-page/list' element={<BoardListPage/>} />
+          <Route path='key-we-board-page/register' element={<BoardRegisterPage/>} />
+          <Route path='key-we-board-page/read/:boardId' element={<BoardReadPage/>} />
+          <Route path='key-we-board-page/modify/:boardId' element={<BoardModifyPage/>} />
+          <Route path='key-we-board-page/sign-in' element={<SignIn/>} />
+          <Route path='key-we-board-page/sign-up' element={<SignUp/>} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
