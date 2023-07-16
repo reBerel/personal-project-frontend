63ae9be3a1c560acee4dd9b80d5f8726c73995d1
Raun0129 <63294294+reBerel@users.noreply.github.com>
2023-07-17 06:05:50 +0900

    update: [장현민]로그인 및 회원가입을 위한 firebase적용[KWF-FIREBASE-1]

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,49 @@ import React, { useEffect } from 'react';
 import MainRouter from './router/MainRouter';
 import NavigationBar from './navigation/NavigationBar'
 import NavigatonBottomBar from './navigation/NavigatonBottomBar'
-import { firestore } from "./firebase";
-const App: React.FC = () =>{
+import "./firebase";
+import { getAuth } from 'firebase/auth';
+import { loginUser } from './api/UserApi';
+import { useNavigate } from 'react-router-dom';
+import useUserStore from './store/UserStore';
+import { User } from './user/entity/User';
+
+const App: React.FC = () => {
+  const navigate = useNavigate();
+  const setUser = useUserStore((state) => state.setUser);
+
   useEffect(() => {
-    console.log(firestore)
-  })
+    getAuth().onAuthStateChanged(async (user) => {
+      if (user) {
+        console.log('firebase : ', user);
+        const result = await loginUser(user.uid);
+        console.log('server : ', result);
+        if (result) {
+          setUser(result);
+          if (window.location.pathname.includes('sign-up') || window.location.pathname.includes('sign-in')) {
+            navigate('/key-we-board-page/list');
+          }
+        } else {
+          alert('회원가입해야함');
+          navigate('/key-we-board-page/sign-up');
+        }
+      } else {
+        setUser({} as User);
+      }
+    });
+  }, []);
+
   return (
-    
+
     <div>
-    <div style={{position: 'relative', minHeight: 100}}>
-        <NavigationBar/>
-        <MainRouter/>
-  
-    </div>
-    <div>
-    <NavigatonBottomBar/>
-    </div>;
+      <div style={{ position: 'relative', minHeight: 100 }}>
+        <NavigationBar />
+        <MainRouter />
+
+      </div>
+      <div>
+        <NavigatonBottomBar />
+      </div>
     </div>
   );
 }
