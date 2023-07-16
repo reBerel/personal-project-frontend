79d3ee3a4e7a6e8fd2079e02aeedde0284b22a49
Raun0129 <63294294+reBerel@users.noreply.github.com>
2023-07-17 06:10:37 +0900

    update: [장현민]firebase를 이용한 로그아웃 구현[KWF-SIGN_OUT-1]

--- src/navigation/NavigationBar.tsx
+++ src/navigation/NavigationBar.tsx
@@ -3,51 +3,58 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
 import { Grid, ThemeProvider, useTheme } from '@mui/material';
 import { Link } from 'react-router-dom';
-
-
-import {Search, SearchIconWrapper, StyledInputBase, AppBar } from './NavigationComponent'
-import {  } from '@mui/material/colors';
+import { Search, SearchIconWrapper, StyledInputBase, AppBar } from './NavigationComponent'
+import '@mui/material/colors';
 import NavigationSideBar from './NavigationSideBar';
+import useUserStore from '../store/UserStore';
+import { getAuth, signOut } from 'firebase/auth';
 
 export default function NavigationBar() {
   const theme = useTheme();
+  const user = useUserStore((state) => state.user);
 
   return (
     <ThemeProvider theme={theme}>
- 
+
       <AppBar position="static" sx={{ backgroundColor: '#6BB07B' }}>
-        <Toolbar>          
-        <Grid container spacing={1} sx={{marginTop: '10px'}}>
-        <Grid item xs={2}>  
-          <Box/>
-          </Grid>
-        <Grid item xs={2}>  
-          <Typography color='white' variant="h6" component={Link} to="/key-we-board-page/list" style={{ textDecoration: "none" }}>
-            KeyWe |
-          </Typography>
-          </Grid>
-          <Grid item xs={3.7}>  
-          <Box/>
-          </Grid>
-          <Search>
-            <SearchIconWrapper />            
-            <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} sx={{padding: '5px'}}/>
-          </Search>          
+        <Toolbar>
+          <Grid container spacing={1} sx={{ marginTop: '10px' }}>
+            <Grid item xs={2}>
+              <Box />
+            </Grid>
+            <Grid item xs={2}>
+              <Typography color='white' variant="h6" component={Link} to="/key-we-board-page/list" style={{ textDecoration: "none" }}>
+                KeyWe |
+              </Typography>
+            </Grid>
+            <Grid item xs={3.7}>
+              <Box />
+            </Grid>
+            <Search>
+              <SearchIconWrapper />
+              <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} sx={{ padding: '5px' }} />
+            </Search>
             <Grid item xs={1.6}>
-          <Button component={Link} to="/key-we-board-page/sign-in" color="inherit">로그인</Button>                    
-          </Grid>
-          <Grid item xs={0.5}>  
-          <NavigationSideBar/>
-          </Grid>
-          <Grid item xs={2}>  
-          <Box/>
-          </Grid>
+              {
+                user.uid ?
+                  <div>
+                    <span>{user.nickName}님 </span>
+                    <Button onClick={() => signOut(getAuth())} color="inherit">로그아웃</Button>
+                  </div>
+                  : <Button component={Link} to="/key-we-board-page/sign-in" color="inherit">로그인</Button>
+              }
+            </Grid>
+            <Grid item xs={0.5}>
+              <NavigationSideBar />
+            </Grid>
+            <Grid item xs={2}>
+              <Box />
+            </Grid>
           </Grid>
         </Toolbar>
-      </AppBar>           
+      </AppBar>
     </ThemeProvider>
   );
 }
