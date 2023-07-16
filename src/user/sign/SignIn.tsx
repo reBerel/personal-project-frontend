48ed61308357bffa01890546fd8c3fd3cd0c3037
Raun0129 <63294294+reBerel@users.noreply.github.com>
2023-07-17 06:17:29 +0900

    update: [장현민]firebase를 이용한 로그인 페이지 구성 [KWF-SIGN_IN-5]

--- src/user/sign/SignIn.tsx
+++ src/user/sign/SignIn.tsx
@@ -1,8 +1,9 @@
 import { Box, Button, Container, Grid, Link, TextField, ThemeProvider, Typography, createTheme } from '@mui/material'
 import { green } from '@mui/material/colors';
-import React, {  useState } from 'react'
-import springAxiosInst from '../../utility/axiosInstance';
-import { User } from '../entity/User';
+import React, { useState } from 'react'
+import { browserLocalPersistence, browserSessionPersistence, getAuth, GoogleAuthProvider, setPersistence, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { FirebaseError } from 'firebase/app';
+import useUserStore from '../../store/UserStore';
 
 const theme = createTheme({
   components: {
@@ -12,10 +13,10 @@ const theme = createTheme({
           padding: ' 10px'
         },
       },
-    },    
+    },
   },
   palette: {
-    primary:{
+    primary: {
       main: green['500'],
     },
   },
@@ -24,68 +25,75 @@ const theme = createTheme({
 const SignIn = () => {
   const [inputEmail, setInputEmail] = useState('')
   const [inputPassword, setInputPassword] = useState('')
-  const [user, setUser] = useState('')
+  const [isAuto, setIsAuto] = useState<boolean>(false)
 
-  const handleInputEmail = (e: any) => {
-    setInputEmail(e.target.value)
-  }
-  
-  const handleInputPassword = (e: any) => {
-    setInputPassword(e.target.value)
+  const handleInputEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputEmail(e.currentTarget.value);
   }
 
-  const onClickSignIn = () => {
+  const handleInputPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputPassword(e.currentTarget.value)
+  }
+
+  const onClickSignIn = async () => {
     console.log('click sign-in')
     console.log('Email: ', inputEmail)
     console.log('Password: ', inputPassword)
-    springAxiosInst.post('/user/sign-in', {
-      user_Email: inputEmail,
-      user_Password: inputPassword
-    })
-    .then(res => {
-      const userData = res.data;
-      setUser(userData);
-      console.log(res);
-      console.log('userData.email :: ',userData.email);
-      console.log('userData.password :: ',userData.password);      
-      if (userData.email === undefined) {
-        console.log('======================',userData.password)
-        alert('입력하신 email이 일치하지 않습니다')        
-      } else if (userData.email === null) {
-        console.log('======================','입력하신 비밀번호 가 일치하지 않습니다.')
-        alert('입력하신 비밀번호 가 일치하지 않습니다.')
-      } else if (userData.email === inputEmail) {
-        console.log('======================','로그인 성공')
-        sessionStorage.setItem('email', inputEmail)
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(auth, inputEmail, inputPassword);
+      console.log(userCredential);
+
+    } catch (error) {
+      const err = error as FirebaseError;
+      console.log(err.message);
+      if (err.message.includes('auth/invalid-email')) {
+        alert('이메일이 없습니다.');
+      } else if (err.message.includes('auth/invalid-password')) {
+        alert('비밀번호가 틀렸습니다.');
+      } else {
+        alert('서버 에러입니다.');
       }
-      // document.location.href = '/key-we-board-page/sign-in'
-    })
-    .catch(error => {
-      console.error(error);
-    });
-  }    
+    }
+  }
+
+  const onClickGoogleSignIn = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      await setPersistence(auth, isAuto ? browserLocalPersistence : browserSessionPersistence);
+      const result = await signInWithPopup(auth, provider);
+      console.log(result);
+    } catch (error) {
+      alert('서버 에러입니다.');
+    }
+  }
+
   return (
     <ThemeProvider theme={theme}>
-    <Container maxWidth="xs"  style={{ marginTop: "30px" }}>
-      <Typography component="h1" variant="h5" sx={{ marginBottom: 1 }}> 로그인 </Typography>
-    <Box display="flex" flexDirection="column" >
-       <TextField autoFocus id='email' name='email' label="아이디" sx={{p:0.8}} value={inputEmail} onChange={handleInputEmail}/>
-       <TextField id='password' name='password' label="비밀번호" sx={{p:0.8}} value={inputPassword} onChange={handleInputPassword}/>
-       <Button onClick={onClickSignIn} style={{marginTop: "20px"}} variant="contained" sx={{backgroundColor:"#6BB07B", color: 'white'}} >로그인</Button>
-       </Box >
-       <Grid container>
-            <Grid item xs marginTop='10px'>
-       <Link href="/key-we-board-page/sign-up" underline="none" fontSize="13px">아이디/비밀번호 찾기</Link>
-       </Grid>
-       <Grid item>
-        <Box marginTop="10px">
-       <Link href="/key-we-board-page/sign-up" underline="none" fontSize="13px">회원가입하기</Link>
-       </Box>
-       </Grid>
-       
-       </Grid>
-       {/* oauth 자리 */}
-    </Container>
+      <Container maxWidth="xs" style={{ marginTop: "30px" }}>
+        <Typography component="h1" variant="h5" sx={{ marginBottom: 1 }}> 로그인 </Typography>
+        <Box display="flex" flexDirection="column" >
+          <TextField autoFocus id='email' name='email' label="아이디" sx={{ p: 0.8 }} value={inputEmail} onChange={handleInputEmail} />
+          <TextField id='password' name='password' label="비밀번호" sx={{ p: 0.8 }} value={inputPassword} onChange={handleInputPassword} />
+          <div>
+            <input type="checkbox" onChange={(e) => { setIsAuto(e.currentTarget.checked); console.log(e.currentTarget.checked); }} /><span>자동로그인</span>
+          </div>
+          <Button onClick={onClickSignIn} style={{ marginTop: "20px" }} variant="contained" sx={{ backgroundColor: "#6BB07B", color: 'white' }} >로그인</Button>
+          <Button onClick={onClickGoogleSignIn} style={{ marginTop: "20px" }} variant="contained" sx={{ backgroundColor: "#198eee", color: 'white' }} >구글 로그인</Button>
+        </Box >
+        <Grid container>
+          <Grid item xs marginTop='10px'>
+            <Link href="/key-we-board-page/sign-up" underline="none" fontSize="13px">아이디/비밀번호 찾기</Link>
+          </Grid>
+          <Grid item>
+            <Box marginTop="10px">
+              <Link href="/key-we-board-page/sign-up" underline="none" fontSize="13px">회원가입하기</Link>
+            </Box>
+          </Grid>
+        </Grid>
+        {/* oauth 자리 */}
+      </Container>
     </ThemeProvider>
   )
 }
