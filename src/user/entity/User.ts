edc2e7906d5eff0da95011673ccb846cb93b9454
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-07-21 16:56:13 +0900

    update: [장현민]유저 entity, state추가 [KWF-USER-x]

--- src/user/entity/User.ts
+++ src/user/entity/User.ts
@@ -1,7 +1,10 @@
+import { Board } from "../../entity/Board";
+
 export interface User {
     userId: number;
     nickName: string;
     name: string;
     email: string;
     uid: string;
+    board: Board;
 }
