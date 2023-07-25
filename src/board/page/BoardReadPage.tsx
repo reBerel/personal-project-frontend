3a38aa39f84da3a6846957ed7f2c988277d82205
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-07-25 17:40:59 +0900

    update: [장현민] 북마크 기능 추가 [KWF-BOOKMARK-3]

--- src/board/page/BoardReadPage.tsx
+++ src/board/page/BoardReadPage.tsx
@@ -11,6 +11,12 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { fetchBoard, incrementReadCount, useBoardQuery } from '../../api/BoardApi';
 import useUserStore from '../../store/UserStore';
 import CommentPage from '../../comment/page/CommentPage';
+import springAxiosInst from '../../utility/axiosInstance';
+import CommentListPage from '../../comment/page/CommentListPage';
+
+type SelectedBoardType = {
+  boardId: number;
+};
 
 const theme = createTheme({
   components: {
@@ -30,24 +36,40 @@ const theme = createTheme({
 });
 
 const BoardReadPage = () => {
-  const { boardId } = useParams()
+  const { boardId } = useParams()  
   const { data: board } = useBoardQuery(boardId || '')
   const [readCount, setReadCount] = useState(board?.readCount || 0);
+  const [selectedBoard, setSelectedBoard] = useState<SelectedBoardType | null>(null);
   const [isBookmarkChecked, setBookmarkChecked] = useState(false);
   const user = useUserStore((state) => state.user)
   const navigate = useNavigate()
-  const handleBookmark = () => {
-    if (user.uid) {
-      setBookmarkChecked((prev: any) => !prev);
+  const handleBookmark = async () => {
+    if (user?.userId) {
+      setBookmarkChecked((prev) => !prev);
+      try {
+        // axios를 사용하여 서버에 북마크 정보 저장 요청 보내기
+        const response = await springAxiosInst.post(`/user/bookmark/${user.userId}`, {
+          boardId: selectedBoard?.boardId,
+        });
+        if (response.status === 200) {
+          // 북마크 상태 업데이트
+          setBookmarkChecked(!isBookmarkChecked);
+        } else {
+          console.error('북마크 저장 실패');
+        }
+      } catch (error) {
+        console.error('북마크 저장 실패:', error);
+      }
     } else {
-      alert("로그인 후 이용가능합니다.")
+      alert('로그인 후 이용 가능합니다.');
     }
-  }
-  const handleModifyClick = () => {
+  };
+  
+  const handleModifyClick = ()=> {
     if (user.uid) {
       navigate(`/key-we-board-page/modify/${boardId}`)
-    } else
-      alert("수정권한이 없는 게시물입니다.")
+    }else
+    alert("수정권한이 없는 게시물입니다.")
   }
   useEffect(() => {
     const fetchBoardData = async () => {
@@ -59,14 +81,14 @@ const BoardReadPage = () => {
   const handleReadCount = async (boardId: string) => {
     await incrementReadCount(boardId);
     setReadCount((prevReadCount) => prevReadCount + 1)
-    console.log("prevReadCount()");
-  }
+    console.log("prevReadCount()");  
+  }      
   useEffect(() => {
     if (boardId) {
       handleReadCount(boardId);
     }
-  }, [boardId])
-
+  },[boardId])      
+    
   return (
     <ThemeProvider theme={theme}>
       <Container maxWidth="md" sx={{ marginTop: '2rem' }}>
@@ -74,20 +96,21 @@ const BoardReadPage = () => {
           <Table aria-label='board table'>
             <TableHead>
               <TableRow key={board?.boardId}>
-                <TableCell align='left'>No. {boardId} </TableCell>
-                <TableCell colSpan={3} align='left'>제목:{board?.title} [{board?.replyCount ? board.replyCount : 0}] </TableCell>
+                <TableCell colSpan={1} align='left'>No. {boardId} </TableCell>
+                <TableCell colSpan={4} align='left'>[{board?.boardCategory ? board.boardCategory: '-'}] 제목:{board?.title} [{board?.replyCount ? board.replyCount : 0}] </TableCell>
                 <TableCell>
                   <Checkbox checked={isBookmarkChecked} onChange={() => handleBookmark()} sx={{ color: green['500'] }} icon={<BookmarkBorderIcon />} checkedIcon={<BookmarkIcon />} />
                 </TableCell>
               </TableRow>
               <TableRow>
                 <TableCell align='left' sx={{ width: '2Z%' }}>작성자:{board?.writer} </TableCell>
-                <TableCell sx={{ width: '30%' }}></TableCell>
-                <TableCell align='center' sx={{ width: '28%' }}>작성일 {board?.createDate} </TableCell>
+                <TableCell sx={{ width: '5%' }}></TableCell>
+                <TableCell sx={{ width: '25%' }}></TableCell>
+                <TableCell align='center'sx={{ width: '28%' }}>작성일 {board?.createDate} </TableCell>
                 <TableCell align='left' sx={{ width: '10%' }}>추천 {board?.likeCount ? board.likeCount : 0} </TableCell>
                 <TableCell align='left' sx={{ width: '8%' }}>
-                  <RemoveRedEyeIcon fontSize='small' />
-                  {readCount}
+                  <RemoveRedEyeIcon fontSize='small'/>
+                  {board?.readCount}
                 </TableCell>
               </TableRow>
             </TableHead>
@@ -95,27 +118,29 @@ const BoardReadPage = () => {
         </TableContainer>
         <TextField disabled name="content" multiline value={board?.content}
           minRows={20} maxRows={30} sx={{ borderRadius: '4px', width: '850px', marginTop: '10px', marginBottom: '5px' }} />
-        {
-          user.uid ?
+          {
+            user.uid?
             (<Button variant='outlined' onClick={handleModifyClick}>수정</Button>)
-            : (
+            :(
               <span></span>
             )
-        }
+          }
         <Grid container spacing={2}>
           <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'right' }}>
             <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} sx={{ display: 'flex', alignItems: 'right', justifyContent: 'right', color: green['500'] }} />
           </Grid>
           <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'left' }}>
+            {/* 게시물 목록에서 각 게시물을 클릭할 때 handleBoardClick 함수를 호출하도록 함 */}
             <Checkbox checked={isBookmarkChecked} onChange={() => handleBookmark()} icon={<BookmarkBorderIcon />} checkedIcon={<BookmarkIcon />} sx={{ display: 'flex', alignItems: 'left', justifyContent: 'left', color: green['500'] }} />
           </Grid>
         </Grid>
-        <TableCell sx={{}}>댓글</TableCell>
-        <CommentPage />
+        <TableCell colSpan={5}>댓글</TableCell>
       </Container>
+        <CommentListPage/>
+        <CommentPage />
       <BoardListPageSub />
     </ThemeProvider>
   )
 }
 
-export default BoardReadPage
+export default BoardReadPage;
--- src/user/page/UserBookmark.tsx
+++ src/user/page/UserBookmark.tsx
@@ -1,14 +1,17 @@
-import { AlertTitle, CircularProgress, Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, createTheme } from '@mui/material'
-import React, { useEffect } from 'react'
+import { AlertTitle, CircularProgress, Container, Pagination, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, createTheme } from '@mui/material'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import { green } from '@mui/material/colors';
-import BoardPageComponent from '../../board/component/BoardPageComponent'
 import useUserStore from '../../store/UserStore';
 import useBoardStore from '../../store/BoardStore';
-import { useNavigate } from 'react-router-dom';
-import { fetchBoardList, useBoardQueryList } from '../../api/BoardApi';
+import { useNavigate, useParams } from 'react-router-dom';
+import { fetchBoardListPage, useBoardQueryList } from '../../api/BoardApi';
 import BookIcon from '@mui/icons-material/Book';
+import { Board, BoardResponse } from '../../board/entity/Board'
+
+const pageNumber = 10;
+const pageSize = 10;
 
 const theme = createTheme({
   components: {
@@ -16,7 +19,7 @@ const theme = createTheme({
       styleOverrides: {
         root: {
           '& .MuiInputBase-root': {
-            padding: '5px 12px',
+            padding: '5px 12px', // 입력 필드의 내부 여백 설정
           },
         },
       },
@@ -37,69 +40,134 @@ const theme = createTheme({
 });
 
 const UserBookmark = () => {
-  const { data: boards, isLoading, isError } = useBoardQueryList()
-  const users = useUserStore((state) => state.users)
-  const setBoards = useBoardStore((state)=> state.setBoards)
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const handlePageChange = (event: any, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const { data: boards, isLoading, isError } = useBoardQueryList();
+  const { boardId } = useParams(); 
+  const users = useUserStore((state) => state.users);
+  const setBoards = useBoardStore((state) => state.setBoards);
+  const [selectedBoard, setSelectedBoard] = useState<Board | null>(null);
   const navigate = useNavigate();
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetchBoardList()
-      setBoards(data)
+
+  const fetchBoardsByPage = async (pageNumber: number) => {
+    try {
+      const data: BoardResponse = await fetchBoardListPage(pageNumber, pageSize);
+      setBoards(data.boards);
+      setTotalPages(data.totalPages);
+    } catch (error) {
+      console.error('게시물 목록을 불러오는데 실패했습니다:', error);
     }
-    fetchData()
-  }, [setBoards])
+  };
+
+  useEffect(() => {
+    // 페이지가 변경될 때마다 데이터를 가져오도록 호출
+    fetchBoardsByPage(page);
+  }, [page]);
+
+  useEffect(() => {
+    // 컴포넌트가 처음 렌더링될 때 데이터를 가져오도록 호출
+    const fetchData = async () => {
+      const data = await fetchBoardListPage(pageNumber, pageSize);
+      setBoards(data.boards);
+      setTotalPages(data.totalPages);
+    };
+    fetchData();
+  }, [setBoards]);
+
   if (isLoading) {
-    return <CircularProgress />
+    return <CircularProgress />;
   }
   if (isError) {
-    return <Typography>리스트를 갖고오는 중 에러 발생!</Typography>
+    return <Typography>리스트를 갖고오는 중 에러 발생!</Typography>;
   }
 
   const ReadClick = (boardId: number) => {
     navigate(`/key-we-board-page/read/${boardId}`)
-  }
+  };
 
   return (    
-      <ThemeProvider theme={theme}>
-        <Container maxWidth="md" style={{ marginTop: '3rem' }}>          
-        <Typography component="h1" variant="h5" sx={{color: '#6BB07B'}}> <BookIcon/> MyBookMark</Typography>
-          <TableContainer component={Paper}>
-            <Table aria-label='board table'>
-              <TableHead>
-                <TableRow>
-                  <TableCell align='center' style={{ width: '7%' }}>No.</TableCell>
-                  <TableCell align='center' style={{ width: '45%' }}>제목</TableCell>
-                  <TableCell align='center' style={{ width: '10%' }}>작성자</TableCell>
-                  <TableCell align='center' style={{ width: '20%' }}>작성일</TableCell>
-                  <TableCell align='center' style={{ width: '7%' }}>추천</TableCell>
-                  <TableCell align='center' style={{ width: '3%' }}>
-                    <RemoveRedEyeIcon fontSize='small' />
+    <ThemeProvider theme={theme}>
+    <Container maxWidth="md" style={{ marginTop: '3rem' }}>
+      <Typography component="h1" variant="h5" sx={{ color: '#6BB07B' }}>
+        {' '}
+        <BookIcon /> MyBookMark
+      </Typography>
+      {/* 선택된 게시물 정보 출력 */}
+      {selectedBoard && (
+        <Typography variant="h6" sx={{ mt: 2 }}>
+          선택된 게시물: {selectedBoard?.title}
+        </Typography>
+      )}
+      <TableContainer component={Paper}>
+        <Table aria-label="board table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center" style={{ width: '7%' }}>
+                No.
+              </TableCell>
+              <TableCell align="center" style={{ width: '45%' }}>
+                제목
+              </TableCell>
+              <TableCell align="center" style={{ width: '10%' }}>
+                작성자
+              </TableCell>
+              <TableCell align="center" style={{ width: '20%' }}>
+                작성일
+              </TableCell>
+              <TableCell align="center" style={{ width: '7%' }}>
+                추천
+              </TableCell>
+              <TableCell align="center" style={{ width: '3%' }}>
+                <RemoveRedEyeIcon fontSize="small" />
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users?.length === 0 ? (
+              <TableRow>
+                <TableCell colSpan={5} align="center">
+                  현재 등록된 게시물이 없습니다.
+                </TableCell>
+              </TableRow>
+            ) : (
+              users?.map((user) => (
+                <TableRow
+                  key={user.userId}
+                  onClick={() => ReadClick(user.userId)}
+                  style={{ cursor: 'pointer' }}
+                >
+                  <TableCell align="center" sx={{ fontSize: '14px' }}>
+                    {user.userId}
+                  </TableCell>
+                  <TableCell align="center" sx={{ fontSize: '14px' }}>
+                    {user.board.title} [{user.board.replyCount ? user.board.replyCount : 0}]
+                  </TableCell>
+                  <TableCell align="center" sx={{ fontSize: '14px' }}>
+                    {user.board.writer}
+                  </TableCell>
+                  <TableCell align="center" sx={{ fontSize: '12px' }}>
+                    {user.board.createDate}
+                  </TableCell>
+                  <TableCell align="center" sx={{ fontSize: '14px' }}>
+                    {user.board.likeCount ? user.board.likeCount : 0}
+                  </TableCell>
+                  <TableCell align="center" sx={{ fontSize: '14px' }}>
+                    {user.board.readCount ? user.board.readCount : 0}
                   </TableCell>
                 </TableRow>
-              </TableHead>
-              <TableBody>
-                {users?.length === 0 ? (
-                  <TableRow>
-                    <TableCell colSpan={5} align='center'>현재 등록된 게시물이 없습니다.</TableCell>
-                  </TableRow>
-                ) : (
-                  users?.map((user) => (
-                    <TableRow key={user.userId} onClick={() => ReadClick(user.userId)} style={{ cursor: 'pointer' }}>
-                      <TableCell align='center' sx={{ fontSize: '14px' }}>{user.userId}</TableCell>
-                      <TableCell align='center' sx={{ fontSize: '14px' }}>{user.board.title} [{user.board.replyCount ? user.board.replyCount : 0}]</TableCell>
-                      <TableCell align='center' sx={{ fontSize: '14px' }}>{user.board.writer}</TableCell>
-                      <TableCell align='center' sx={{ fontSize: '12px' }}>{user.board.createDate}</TableCell>
-                      <TableCell align='center' sx={{ fontSize: '14px' }}>{user.board.likeCount ? user.board.likeCount : 0}</TableCell>
-                      <TableCell align='center' sx={{ fontSize: '14px' }}>{user.board.readCount ? user.board.readCount : 0}</TableCell>
-                    </TableRow>
-                  )))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <BoardPageComponent/>
-        </Container>
-      </ThemeProvider>
+              ))
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+        <Pagination page={page} count={totalPages} siblingCount={2} boundaryCount={1} shape="rounded" onChange={handlePageChange} showFirstButton showLastButton sx={{ marginTop: '0.5rem' }}/>
+      </Container>
+    </ThemeProvider>
   )
 }
 
-export default UserBookmark
+export default UserBookmark;
