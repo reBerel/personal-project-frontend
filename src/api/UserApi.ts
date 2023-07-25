46014ec55b35ce8a74715e929095ff7f35ed12d4
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-07-25 17:28:34 +0900

    update: [장현민]회원탈퇴 로직api 완성[KWF-SIGNDOWN-1]

--- src/api/UserApi.ts
+++ src/api/UserApi.ts
@@ -9,6 +9,12 @@ export const bookmarkChecking = async (uid: string): Promise<User> => {
   const result = await springAxiosInst.get<User>(`/user/bookmark/${uid}`)
   return result.data;
 }
-export const userDeleteHandler = async(userId: string): Promise<void> => {
-  await springAxiosInst.delete(`/user/${userId}`)
-}
+export const userDeleteHandler = async (userId: string): Promise<void> => {
+  try {
+    const parsedUserId = parseInt(userId, 10); // 문자열을 숫자로 변환
+    await springAxiosInst.delete(`/user/${parsedUserId}`);
+    console.log('회원 탈퇴가 완료되었습니다.');
+  } catch (error) {
+    console.error('회원 탈퇴 중에 오류가 발생했습니다.', error);
+  }
+};
